dac77cbd180d0de88409fa179d84fba7f3bd0caa
furuhama <furuhama@users.noreply.github.com>
2018-04-21 14:50:01 +0900

    Add fn greet to lib.rs & Build as release

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,16 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum Cell {
